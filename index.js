e07dd3950b761fb51720c557ea4556d5836a1a16
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-10-26 14:14:24 +0000

    complete all problems

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 //1
 function countToTwenty(){
-  
+  for (let i =1; i<21; i++){
+      console.log(i);
+  }
 }
-
+//countToTwenty();
 //2
 function countFromTwenty(){
-  
+  for(let i = 20; i>0; i--){
+      console.log(i);
+  }
 }
-
+//countFromTwenty();
 //3
-function countToN(){
-  
+function countToN(num){
+  for(let i=1;i<num+1;i++){
+      console.log(i);
+  }
 }
-
+//countToN(10);
 
 //4
-function countFromN(){
-  
+function countFromN(num){
+  for(let i=num; i>0;i--){
+      console.log(i);
+  }
+}
+//countFromN(10);
+//5
+function countEveryOdd(num){
+  for (let i = 1; i<=num;i++){
+      if(i%2 != 0){
+          console.log(i);
+      }
+    }
 }
 
-//5
-function countEveryOdd(){
-  
-}
+countEveryOdd(10);
